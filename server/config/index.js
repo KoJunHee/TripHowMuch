c912df7219e2f3ab687004015f7c7f5b378b8b6d
junhee.ko <junhee.ko@ikoob.com>
2017-11-03 22:39:30 +0900

    finish

--- server/config/index.js
+++ server/config/index.js
@@ -1,29 +1,29 @@
 var config = {
     name: 'API',
     server: {
-        host: 'host',
-        port: 'port',
+        // host: ‘172.31.7.75’,
+        host: '0.0.0.0',
+        port: 8000,
         labels: ['api'],
         routes: {
-            cors: { credentials: `true` }
+            cors: { credentials: 'true' }
         }
     },
     openApi: {
-        baseUrl: 'baseUrl',
-        myKey: 'myKey'
+        baseUrl: 'http://api.visitkorea.or.kr/openapi/service',
+        myKey: 'Q227VR0L46ig9PXHPpQ4luY1jioIsfxFQobXshqMsW3wC2zZ4j5k18mBFLAoZkLSdjtRa49wW4z%2B1z9ZOfTMAA%3D%3D'
     },
     database: {
         mongo:
         {
-            host: 'host',
-            username: 'username',
-            password: 'password',
-            port: 'port',
-            database: 'database' 
+            host: 'ds123311.mlab.com',
+            username: 'junhee.ko',
+            password: 'qq1212qq1212!',
+            port: '23311',
+            database: 'triphowmuch'
         }
     }
 };
 
 
 module.exports = config;
-
